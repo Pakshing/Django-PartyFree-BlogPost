3d7105584dd09e06666afc38a79bccf68b1d834a
Pakshing <55418556+Pakshing@users.noreply.github.com>
2020-01-16 15:11:42 -0800

    Update tasks.py

--- APP_01_PartyFree/tasks.py
+++ APP_01_PartyFree/tasks.py
@@ -2,8 +2,8 @@ from celery import shared_task
 from time import sleep
 from twilio.rest import Client
 from twilio.twiml.voice_response import Play, VoiceResponse
-account_sid = 'AC94a0517d20fc9a6ad2eee9d5423791b7'
-auth_token = 'c135b809899593c0b8317a57ce5b9a0b'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 CallFrom = "+19167961080"
 
